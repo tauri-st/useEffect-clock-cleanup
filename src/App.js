c8d5d9715be25ffc1db46a51459ab8c744f93fbd
tauri_s <tauristclaire@gmail.com>
2023-12-21 14:26:32 -0800

    Starter code and dependencies

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Clock from "./Clock";
+
+function App() {
+  return (
+    <div className="App">
+      <Clock />
+      <button className="clock-toggle">Show clock</button>
+    </div>
+  );
+}
+
+export default App;
