3a316ef64ab8f496b94608094a67e8b3465c2a69
tauri_s <tauristclaire@gmail.com>
2023-12-21 15:14:18 -0800

    Add onClick handler to the button in App js to change state of showClock

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ function App() {
 
   const [showClock, setShowClock] = useState(true);
 
+  const toggleClock = (e) => {
+    showClock === true ? setShowClock(false) : setShowClock(false);
+  }
+
   return (
     <div className="App">
       <Clock />
-      <button className="clock-toggle">Show clock</button>
+      <button className="clock-toggle" onClick={toggleClock}>Show clock</button>
     </div>
   );
 }
